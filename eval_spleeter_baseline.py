110b1d357d0a5b92fe0287f4cc6b786f84b2aa4a
alia <alia.morsi@aucegypt.edu>
2022-10-16 20:50:12 +0200

    added spleeter eval code, and modified both eval files for better result saving

--- eval_spleeter_baseline.py
+++ eval_spleeter_baseline.py
@@ -1,5 +1,3 @@
-#Todo: have a different eval.py for each model variant.
-# not sure if train.py will have the same fate
 import torch
 import numpy as np
 import argparse
@@ -14,10 +12,16 @@ from asteroid.complex_nn import torch_complex_from_magphase
 import os
 import warnings
 import sys
+import pandas as pd
 
-from eval import load_model, separate, inference_args
+from spleeter.separator import Separator
+from spleeter.audio.adapter import AudioAdapter
 
-#This expects the test files to be stored in the same file structure of the leakage dataset (variants from 1-10, etc). For running test files kept in other structures, please build on eval_one instead. 
+separator = Separator('spleeter:4stems-16kHz')
+
+
+from eval import inference_args
+
 
 def eval_main(
     root,
@@ -27,7 +31,6 @@ def eval_main(
     softmask=False,
     residual_model=False,
     model_path='.',
-    model_name="leakage_xumx",
     outdir=None,
     start=0.0,
     duration=-1.0,
@@ -37,12 +40,6 @@ def eval_main(
     variant='no_concat',
 ):
 
-    #outdir = os.path.join(os.path.abspath(outdir), test_output_files)
-    model_name = os.path.join(model_path, model_name)
-
-    if not (os.path.exists(model_name)):
-        print("model does not exist: {}. Please update path in cnf/eval.yml".format(model_name), file=sys.stderr)
-        quit()
 
     if os.path.exists(outdir):
         print("Results of previous run saved in your chosen outdir: {}, please choose another location".format(outdir), file=sys.stderr)
@@ -58,14 +55,12 @@ def eval_main(
 
     use_cuda = not no_cuda and torch.cuda.is_available()
     device = torch.device("cuda" if use_cuda else "cpu")
-    model, instruments = load_model(variant, model_name, device)
-    
+
     test_dataset = musdb.DB(root=root, subsets="test", is_wav=True, instrument=instrument, data_path=eval_data_path)
     results = museval.EvalStore()
     results_df = pd.DataFrame(columns=['target', 'metric', 'mean_values', 'median_values', 'variant', 'track_name'])
     txtout = os.path.join(outdir, "results.txt")
     fp = open(txtout, "w")
-    
     for track in test_dataset:
         input_file =  os.path.join(os.path.dirname(track.path), "degraded_audio_mix.wav")
         clean_backing_track = os.path.join(os.path.dirname(track.path), "clean_backing_track.wav")
@@ -81,6 +76,10 @@ def eval_main(
             # set to None for reading complete file
             stop = None
 
+        spleeter_adapted_loader = AudioAdapter.default()
+        sample_rate = 44100
+        waveform, _ = spleeter_adapted_loader.load(input_file, sample_rate=sample_rate)
+
         audio, rate = sf.read(input_file, always_2d=True, start=start, stop=stop)
         
         if audio.shape[1] > 2:
@@ -95,29 +94,18 @@ def eval_main(
             # if we have mono, let's duplicate it
             # as the input of OpenUnmix is always stereo
             audio = np.repeat(audio, 2, axis=1)
-
-        if variant == 'concat_1' or 'concat_2':
-            clean_bk_track, rate = sf.read(clean_backing_track, always_2d=True, start=start, stop=stop)
-            shortest = np.min([audio.shape[0], clean_bk_track.shape[0]]) #since we are assuming stereo audio, so the audio length is on the second dim.
-            clean_bk_track = torch.tensor(clean_bk_track)
-            audio = torch.tensor(audio)
-
-            clean_bk_track = torch.narrow(clean_bk_track, 0, 0, shortest)
-            audio = torch.narrow(audio, 0, 0, shortest)
-
-            audio = torch.concat([audio, clean_bk_track], axis=1) #even if the prediction only needs a the first 2 audio channels 
       
-        estimates = separate(
-            audio,
-            model,
-            instruments,
-            niter=niter,
-            alpha=alpha,
-            softmask=softmask,
-            residual_model=residual_model,
-            device=device,
-            variant=variant
-        )
+        prediction = separator.separate(waveform)
+
+        estimates = {}
+        #adapt the output of spleeter:
+        if instrument == 'bass':
+            estimates['degraded_instrument_track'] = prediction['bass']
+            estimates['degraded_backing_track'] = prediction['vocals'] + prediction['other'] + prediction['drums']
+
+        elif instrument == 'drums':
+            estimates['degraded_instrument_track'] = prediction['drums']
+            estimates['degraded_backing_track'] = prediction['vocals'] + prediction['other'] + prediction['bass']
 
         variant_number = os.path.basename(os.path.split(track.path)[0])
         output_path = Path(os.path.join(outdir, instrument, track.name, variant_number))
@@ -127,10 +115,10 @@ def eval_main(
         print(track.name, file=fp)
         for target, estimate in estimates.items():
             sf.write(str(output_path / Path(target).with_suffix(".wav")), estimate, samplerate)
-        
+     
+         
         track_scores = museval.eval_mus_track(track, estimates)
         track_scores.df.to_csv(os.path.join(output_path, 'frame_result.csv'))
-    
         
         summary_target = ['degraded_backing_track', 'degraded_instrument_track']
         summary_metrics = ['SDR', 'SIR', 'SAR']
@@ -143,27 +131,26 @@ def eval_main(
         for t in summary_target:
             for m in summary_metrics:
                 summary_target_col.append(t)
-                summary_metric_col.append(m)
+                summary_metric_col.append(m) 
                 rel_cols = track_scores.df[(track_scores.df['metric'] == m) & (track_scores.df['target'] == t)]
                 summary_metric_median.append(track_scores.frames_agg(rel_cols['score']))
                 summary_metric_mean.append(np.nanmean(rel_cols['score']))
                 track_variant.append(os.path.basename(os.path.dirname(track.path)))
                 track_name.append(track_scores.track_name)
-
+                
         summary_df = pd.DataFrame({ 'target': summary_target_col,
-                                    'metric': summary_metric_col,
+                                    'metric': summary_metric_col, 
                                     'mean_values': summary_metric_mean,
                                     'median_values': summary_metric_median,
                                     'variant': track_variant,
                                     'track_name': track_name
                         })
-
+       
         summary_df.to_csv(os.path.join(output_path, 'results_summary.csv'))
         results.add_track(track_scores.df)
         results_df = results_df.append(summary_df)
         print(track_scores, file=sys.stderr)
         results_df.to_csv(os.path.join(outdir, instrument, 'all_result_summaries.csv'))
-
 
     #aggregate results of all runs
     print(results, file=sys.stderr)
@@ -171,7 +158,6 @@ def eval_main(
     results.frames_agg = "mean"
     print(results, file=sys.stderr)
     fp.close()
-
 
 if __name__ == "__main__":
     # Training settings
@@ -206,7 +192,7 @@ if __name__ == "__main__":
     import yaml
     from asteroid.utils import prepare_parser_from_dict, parse_args_as_dict
 
-    with open("cfg/eval.yml") as f:
+    with open("cfg/eval_baseline.yml") as f:
         eval_conf = yaml.safe_load(f)
     eval_parser = prepare_parser_from_dict(eval_conf, parser=parser)
 
@@ -223,7 +209,6 @@ if __name__ == "__main__":
         softmask=args.softmask,
         niter=args.niter,
         residual_model=args.residual_model,
-        model_name=plain_args.model_name,
         model_path=plain_args.model_path, 
         outdir=plain_args.output_path,
         start=args.start,
